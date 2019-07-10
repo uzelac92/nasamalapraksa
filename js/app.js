18a49b37b29554248187fbe2ac32b8fdfb5d5098
Vladimir Uzelac <vladimiruzelac@Vladimirs-MacBook-Pro.local>
2019-07-11 00:19:46 +0200

    toastr

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('NasaMalaPraksa', ['ngRoute','toastr','ngAnimate','updateMeta']);
+var app = angular.module('NasaMalaPraksa', ['ngRoute','ngAnimate','updateMeta']);
 
 window.fbAsyncInit = function() {
   FB.init({
