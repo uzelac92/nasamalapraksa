06024a2e386ea1c5c793aef5b55e4fbd1b2d263e
Vladimir Uzelac <vladimiruzelac@Vladimirs-MacBook-Pro.local>
2019-07-11 02:46:07 +0200

    over

--- api/post/read_one.php
+++ api/post/read_one.php
@@ -12,47 +12,62 @@ include_once '../config/database.php';
 // get database connection
 $database = new Database();
 $db = $database->getConnection();
- 
-// set ID property of record to read
+
 $id = $_GET['id'];
 
-$query = "SELECT * FROM POST WHERE ID = ? LIMIT 0,1";
-     
-// prepare query statement
+$query = "UPDATE POST SET KLIK = KLIK + 1 WHERE ID = $id;";
 $stmt = $db->prepare( $query );
-
-// bind id of product to be updated
-$stmt->bindParam(1, $id);
-
-// execute query
+$stmt->execute();
+$query = "SELECT * FROM POST WHERE ID = $id LIMIT 0,1;";
+$stmt = $db->prepare( $query );
 $stmt->execute();
 
 // get retrieved row
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 // set values to object properties
+$ID = $row['ID'];
 $NASLOV = $row['NASLOV'];
 $INTRO = $row['INTRO'];
 $SLIKA = $row['SLIKA'];
-$PUTANJA = $row['PUTANJA'];
+$ALT = $row['ALT'];
 $KLIK = $row['KLIK'];
 $KEYWORDS = $row['KEYWORDS'];
-$NASLOV_A = $row['NASLOV_A'];
-$NASLOV_B = $row['NASLOV_B'];
  
-if($NASLOV!=null){
+if($row['NASLOV'] != null){
+
+    $query2 = "SELECT * FROM pitanje WHERE POST_ID = $ID
+            ORDER BY PITANJEID DESC";
+
+    $stmt2 = $db->prepare($query2);
+
+    $stmt2->execute();
+
+    $posts_arr=array();
+    $posts_arr["records"]=array();
+ 
+    while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)){
+
+        $post_item=array(
+            "PITANJEID" => html_entity_decode($row2['PITANJEID']),
+            "UPITNICA" => html_entity_decode($row2['UPITNICA']),
+            "UVOD" => html_entity_decode($row2['UVOD']),
+            "ODGOVOR" => html_entity_decode($row2['ODGOVOR']),
+            "POST_ID" => html_entity_decode($row2['POST_ID']),
+        );
+ 
+        array_push($posts_arr["records"], $post_item);
+    }
+
     // create array
     $post_arr = array(
         "ID" =>  $id,
         "NASLOV" => $NASLOV,
         "INTRO" => $INTRO,
         "SLIKA" => $SLIKA,
-        "PUTANJA" => $PUTANJA,
+        "ALT" => $ALT,
         "KLIK" => $KLIK,
-        "KEYWORDS" => $KEYWORDS,
-        "NASLOV_A" => $NASLOV_A,
-        "NASLOV_B" => $NASLOV_B
- 
+        "PITANJA" => $posts_arr,
     );
  
     // set response code - 200 OK
